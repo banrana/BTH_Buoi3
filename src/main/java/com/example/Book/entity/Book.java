dfd322029260e2c17edaa4155a31759fd3f92f53
Banrana <Blazed1347@gmail.com>
2023-06-03 23:38:49 +0700

    Nop bai buoi 5

--- src/main/java/com/example/Book/entity/Book.java
+++ src/main/java/com/example/Book/entity/Book.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 @Data
 @Entity
 @Table(name = "Book")
